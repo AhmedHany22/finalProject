254bdfd69619a9df01f61fd811d35f9354f4e5b4
Nancy Ahmed <nancyahmed2025@gmail.com>
2021-03-06 09:12:21 +0200

    add new component

---
+++ Techno/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,123 @@
+// import "./Dashboard.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+
+// const Dashboard = () => {
+  
+//   return (
+//     <>
+//    <div>
+//   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+//   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css" rel="stylesheet" />
+//   <div className="main-content">
+//     <div className="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+//       <div className="container-fluid">
+//         <h2 className="mb-5 text-white">Stats Card</h2>
+//         <div className="header-body">
+//           <div className="row">
+//             <div className="col-xl-3 col-lg-6">
+//               <div className="card card-stats mb-4 mb-xl-0">
+//                 <div className="card-body">
+//                   <div className="row">
+//                     <div className="col">
+//                       <h5 className="card-title text-uppercase text-muted mb-0">Traffic</h5>
+//                       <span className="h2 font-weight-bold mb-0">350,897</span>
+//                     </div>
+//                     <div className="col-auto">
+//                       <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+//                         <i className="fas fa-chart-bar" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <p className="mt-3 mb-0 text-muted text-sm">
+//                     <span className="text-success mr-2"><i className="fa fa-arrow-up" /> 3.48%</span>
+//                     <span className="text-nowrap">Since last month</span>
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="col-xl-3 col-lg-6">
+//               <div className="card card-stats mb-4 mb-xl-0">
+//                 <div className="card-body">
+//                   <div className="row">
+//                     <div className="col">
+//                       <h5 className="card-title text-uppercase text-muted mb-0">New users</h5>
+//                       <span className="h2 font-weight-bold mb-0">2,356</span>
+//                     </div>
+//                     <div className="col-auto">
+//                       <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+//                         <i className="fas fa-chart-pie" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <p className="mt-3 mb-0 text-muted text-sm">
+//                     <span className="text-danger mr-2"><i className="fas fa-arrow-down" /> 3.48%</span>
+//                     <span className="text-nowrap">Since last week</span>
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="col-xl-3 col-lg-6">
+//               <div className="card card-stats mb-4 mb-xl-0">
+//                 <div className="card-body">
+//                   <div className="row">
+//                     <div className="col">
+//                       <h5 className="card-title text-uppercase text-muted mb-0">Sales</h5>
+//                       <span className="h2 font-weight-bold mb-0">924</span>
+//                     </div>
+//                     <div className="col-auto">
+//                       <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+//                         <i className="fas fa-users" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <p className="mt-3 mb-0 text-muted text-sm">
+//                     <span className="text-warning mr-2"><i className="fas fa-arrow-down" /> 1.10%</span>
+//                     <span className="text-nowrap">Since yesterday</span>
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="col-xl-3 col-lg-6">
+//               <div className="card card-stats mb-4 mb-xl-0">
+//                 <div className="card-body">
+//                   <div className="row">
+//                     <div className="col">
+//                       <h5 className="card-title text-uppercase text-muted mb-0">Performance</h5>
+//                       <span className="h2 font-weight-bold mb-0">49,65%</span>
+//                     </div>
+//                     <div className="col-auto">
+//                       <div className="icon icon-shape bg-info text-white rounded-circle shadow">
+//                         <i className="fas fa-percent" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <p className="mt-3 mb-0 text-muted text-sm">
+//                     <span className="text-success mr-2"><i className="fas fa-arrow-up" /> 12%</span>
+//                     <span className="text-nowrap">Since last month</span>
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     {/* Page content */}
+//   </div>
+//   <footer className="footer">
+//     <div className="row align-items-center justify-content-xl-between">
+//       <div className="col-xl-6 m-auto text-center">
+//         <div className="copyright">
+//           <p>Made with <a href="https://www.creative-tim.com/product/argon-dashboard" target="_blank">Argon Dashboard</a> by Creative Tim</p>
+//         </div>
+//       </div>
+//     </div>
+//   </footer>
+// </div>
+
+//     </>
+//   )
+      
+// };
+// export default Dashboard;
